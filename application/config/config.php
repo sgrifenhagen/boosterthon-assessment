9cbc1acb727d7e2468bc8f240b709c2a3d1d9a9c
Stuart Grifenhagen <stuart.grifenhagen@gmail.com>
2018-04-26 18:23:13 -0400

    changed site url to boostertest.grifenhagen.info

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://booster.lamp.local/';
+$config['base_url'] = 'http://boostertest.grifenhagen.info/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/view_process.php
+++ application/views/view_process.php
@@ -60,19 +60,3 @@ $btnReview = array(
     </div>
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
-<div class="modal fade" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Modal title</h4>
-            </div>
-            <div class="modal-body">
-                <p>One fine body&hellip;</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
